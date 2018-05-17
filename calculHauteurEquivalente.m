e627fba352c52130a35c4a43a3dd868b2b8afc1b
mdenonci <maximedenoncin@gmail.com>
2018-05-17 22:26:35 +0200

    Bug corrigé : le problème venait de hauteur équivalente mise à 1 si sin(theta) = 0
    Aucune solution trouvée - hauteur équivalente mise à 0.01 pour toute position

--- calculHauteurEquivalente.m
+++ calculHauteurEquivalente.m
@@ -1,6 +1,4 @@
 function hauteurEquivalente=calculHauteurEquivalente(lambda,theta)
-hauteurEquivalente= -1*lambda*(1/pi)*(cos((pi/2)*cos(theta)))/((sin(theta)^2));
-if(sin(theta)==0)
-    hauteurEquivalente=1;
-end
+%hauteurEquivalente= -1*lambda*(1/pi)*(cos((pi/2)*cos(theta)))/((sin(theta)^2));
+hauteurEquivalente = 0.01;
 end
